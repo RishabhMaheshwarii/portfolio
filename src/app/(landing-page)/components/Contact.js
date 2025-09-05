dab088a2e957c69d67821e600eca1db7fa4f210d
Siddharth Thakur <u803760@gmail.com>
2025-09-05 12:58:44 +0530

    changed in the contact page

--- src/app/(landing-page)/components/Contact.js
+++ src/app/(landing-page)/components/Contact.js
@@ -1,4 +1,3 @@
-"use client";
 import { Linkedin, Youtube } from "lucide-react";
 
 const Contact = () => {
--- src/app/(landing-page)/contact/page.js
+++ src/app/(landing-page)/contact/page.js
@@ -7,6 +7,7 @@ export default function ContactPage() {
         <>
             <Navbar />
             <Contact />
+            <Footer />
         </>
     );
 }
